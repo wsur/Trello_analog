ab9c85b18d6f454b62e2a0ec5f7d84699b24d956
Evgeny Plotnikov <jakejo457@gmail.com>
2023-09-25 09:56:42 +0500

    Trello analog, version 1_00

---
+++ app.js
@@ -0,0 +1,46 @@
+const item = document.querySelector('.item')
+const placeholders = document.querySelectorAll('.placeholder');
+
+
+item.addEventListener('dragstart', dragstart);
+item.addEventListener('dragend', dragend);
+
+for(const placeholder of placeholders){
+    placeholder.addEventListener('dragover', dragover);
+    placeholder.addEventListener('dragenter', dragenter);
+    placeholder.addEventListener('dragleave', dragleave);
+    placeholder.addEventListener('drop', dragdrop);
+}
+
+
+function dragstart(event){
+    event.target.classList.add('hold');
+    setTimeout(() => {
+        event.target.classList.add('hide'), 0
+    })
+}
+
+function dragend(event){
+    //event.target.classList.remove('hold', 'hide');
+    event.target.className = 'item';
+}
+
+function dragover(event){
+    event.preventDefault();
+    console.log('drag over');
+}
+function dragenter(event){
+    event.target.classList.add('hovered');
+    console.log('drag enter');
+}
+
+function dragleave(event){
+    event.target.classList.remove('hovered');
+    console.log('drag leave');
+}
+
+function dragdrop(event){
+    event.target.classList.remove('hovered');
+    event.target.append(item);
+    console.log('drag drop');
+}
